bdd257ecc8e2fa34cbc64b93424259b8e255b3e7
hookstdev <112311475+hookstdev@users.noreply.github.com>
2022-09-21 15:50:10 +0530

    Update linux.sh

--- OS/Linux/linux.sh
+++ OS/Linux/linux.sh
@@ -1,4 +1,4 @@
-!# /usr/bin/bash
+#!/bin/bash
 
 sudo pacman -S --noconfirm redshift tlp bleachbit electronmail-bin flameshot-git kotatogram-desktop-bin noisetorch-git protonvpn-gui whatsie-git webcord-git libnetfilter_queue libappindicator-gtk3 linux-g14 linux-g14-headers
 paru backintime
